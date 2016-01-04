48bc5936a6698f1efaab6c1364bc6aee146f3ef4
zhouyantong <zhouyantong@meiyebang.com>
2016-01-04 22:22:21 +0800

    Initial Commit

---
+++ SeletorView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SeletorView
+//
+//  Created by 周彦彤 on 16/1/4.
+//  Copyright © 2016年 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SeletorView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SeletorView
+//
+//  Created by 周彦彤 on 16/1/4.
+//  Copyright © 2016年 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
